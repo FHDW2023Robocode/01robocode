49388644b9988269c4599b54e61ca39fda8652e8
nicojankowski <njnicojankowski128@gmail.com>
2023-06-17 12:51:43 +0200

    import advanced robot

--- src/main/java/de/fhdw/robocode/FirstRobot01.java
+++ src/main/java/de/fhdw/robocode/FirstRobot01.java
@@ -3,14 +3,22 @@ package de.fhdw.robocode;
 import robocode.HitByBulletEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
+import robocode.util.Utils;
+import robocode.AdvancedRobot;
 
-public class FirstRobot01 extends Robot {
+public class FirstRobot01 extends AdvancedRobot {
 
     @Override
     public void run() {
 
         double radius = 100.0;
         double angle = 90.0;
+
+        //make idividually rotateable
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+
+        setTurnRadarRight(Double.POSITIVE_INFINITY);
 
         while (true) {
             ahead(radius);
@@ -20,8 +28,9 @@ public class FirstRobot01 extends Robot {
         }
     }
 
+
     public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+        setTurnRadarRight(2.0 * Utils.normalRelativeAngleDegrees(getHeading() + e.getBearing() - getRadarHeading()));
     }
 
     public void onHitByBullet(HitByBulletEvent e) {
