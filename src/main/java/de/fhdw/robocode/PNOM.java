93e948a0d21e6e2da61d9262e1cdec4f49a587ed
nicojankowski <njnicojankowski128@gmail.com>
2023-06-17 15:08:10 +0200

    update movement

--- src/main/java/de/fhdw/robocode/PNOM.java
+++ src/main/java/de/fhdw/robocode/PNOM.java
@@ -4,11 +4,13 @@ import robocode.*;
 import robocode.util.Utils;
 
 public class PNOM extends AdvancedRobot {
+    int moveDirection = 1;
 
     @Override
     public void run() {
 
         setTurnRadarRightRadians(Double.POSITIVE_INFINITY);
+
         do {
             // Check for new targets.
             // Only necessary for Narrow Lock because sometimes our radar is already
@@ -16,7 +18,7 @@ public class PNOM extends AdvancedRobot {
             // it to turn, so the system doesn't automatically call scan() for us
             // [see the javadocs for scan()].
             //scan();
-            ahead(20);
+            move();
             //setTurnLeft(90)
             setTurnRadarRight(10);
         } while (true);
@@ -24,6 +26,11 @@ public class PNOM extends AdvancedRobot {
 
 
     public void onScannedRobot(ScannedRobotEvent e) {
+        String enemyName = e.getName(); // Get the name of the scanned enemy
+        double enemyBearing = e.getBearing(); // Get the bearing of the scanned enemy
+        double enemyDistance = e.getDistance(); // Get the distance to the scanned enemy
+
+
         double radarTurn =
                 // Absolute bearing to target
                 getHeadingRadians() + e.getBearingRadians()
@@ -41,5 +48,18 @@ public class PNOM extends AdvancedRobot {
     public void onHitByBullet(HitByBulletEvent e) {
         back(30);
         setTurnLeft(30);
+    }
+    public void move(){
+
+
+        if (getTime() % 13 == 0) {
+            moveDirection *= -1; // Reverse the movement direction every 20 turns
+        }
+
+        // Move in the current direction
+        ahead(100 * moveDirection); // Move forward 100 units or backward 100 units based on the moveDirection
+
+        // Turn the robot slightly to create a smoother movement pattern
+        turnRight(8 * moveDirection);
     }
 }
