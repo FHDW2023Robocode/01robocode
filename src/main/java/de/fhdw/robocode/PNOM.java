ad82ecd2421da2cbf7b583ebf5a6549969aabecc
Ole J. Berg <Ole_Julius.Berg@edu.fhdw.de>
2023-06-17 15:37:05 +0200

    best version yet lets goooooo

--- src/main/java/de/fhdw/robocode/PNOM.java
+++ src/main/java/de/fhdw/robocode/PNOM.java
@@ -5,7 +5,8 @@ import robocode.util.Utils;
 
 public class PNOM extends AdvancedRobot {
     int moveDirection = 1;
-
+    double x= 0,y = 0;
+    int counter = 10;
     @Override
     public void run() {
 
@@ -20,7 +21,7 @@ public class PNOM extends AdvancedRobot {
             //scan();
             move();
             //setTurnLeft(90)
-            setTurnRadarRight(10);
+            setTurnRadarRight(45);
         } while (true);
     }
 
@@ -42,7 +43,7 @@ public class PNOM extends AdvancedRobot {
         setTurnGunRightRadians(
                 robocode.util.Utils.normalRelativeAngle(absoluteBearing -
                         getGunHeadingRadians()));
-        fire(3-(getEnergy()/34));
+        fire(getEnergy()/34);
     }
 
     public void onHitByBullet(HitByBulletEvent e) {
@@ -50,16 +51,28 @@ public class PNOM extends AdvancedRobot {
         setTurnLeft(30);
     }
     public void move(){
+        if(x==getX()&&y==getY()&&counter<10) {
+            counter++;
+        }else if(counter>10){
+            back(30);
+            if(getHeading()>180){
+                turnRight(90+90*Math.random());
+            }else {
+                turnLeft(90+90*Math.random());
+            }
+            counter=0;
+        } else{
+            if (getTime() % 13 == 0) {
+                moveDirection *= -1; // Reverse the movement direction every 20 turns
+            }
 
+            // Move in the current direction
+            ahead(100 * moveDirection); // Move forward 100 units or backward 100 units based on the moveDirection
 
-        if (getTime() % 13 == 0) {
-            moveDirection *= -1; // Reverse the movement direction every 20 turns
+            // Turn the robot slightly to create a smoother movement pattern
+            turnRight(8 * moveDirection);
         }
-
-        // Move in the current direction
-        ahead(100 * moveDirection); // Move forward 100 units or backward 100 units based on the moveDirection
-
-        // Turn the robot slightly to create a smoother movement pattern
-        turnRight(8 * moveDirection);
+        x=getX();
+        y=getY();
     }
 }
