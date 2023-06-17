39e0367466b53040652521427cbfc133de99b10b
Ole J. Berg <Ole_Julius.Berg@edu.fhdw.de>
2023-06-17 14:46:52 +0200

    Oles Bot

--- src/main/java/de/fhdw/robocode/FirstRobot01.java
+++ src/main/java/de/fhdw/robocode/FirstRobot01.java
@@ -1,45 +1,48 @@
 package de.fhdw.robocode;
 
 import robocode.HitByBulletEvent;
-import robocode.Robot;
+import robocode.AdvancedRobot;
 import robocode.ScannedRobotEvent;
 import robocode.util.Utils;
-import robocode.AdvancedRobot;
 
 public class FirstRobot01 extends AdvancedRobot {
 
     @Override
     public void run() {
 
-        double radius = 100.0;
-        double angle = 90.0;
+        turnRadarRightRadians(Double.POSITIVE_INFINITY);
+        do {
+            // Check for new targets.
+            // Only necessary for Narrow Lock because sometimes our radar is already
+            // pointed at the enemy and our onScannedRobot code doesn't end up telling
+            // it to turn, so the system doesn't automatically call scan() for us
+            // [see the javadocs for scan()].
+            //scan();
+            ahead(20);
+            //setTurnLeft(90)
+            setTurnRadarRight(10);
 
-        //make idividually rotateable
-        setAdjustGunForRobotTurn(true);
-        setAdjustRadarForGunTurn(true);
-
-        setTurnRadarRight(Double.POSITIVE_INFINITY);
-
-        while (true) {
-            ahead(radius);
-            turnRight(angle);
-            turnRadarRight(angle);
-        }
+        } while (true);
     }
 
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        setTurnRadarRight(2.0 * Utils.normalRelativeAngleDegrees(getHeading() + e.getBearing() - getRadarHeading()));
+        double radarTurn =
+                // Absolute bearing to target
+                getHeadingRadians() + e.getBearingRadians()
+                        // Subtract current radar heading to get turn required
+                        - getRadarHeadingRadians();
+
+        setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn));
         double absoluteBearing = getHeadingRadians() + e.getBearingRadians();
         setTurnGunRightRadians(
                 robocode.util.Utils.normalRelativeAngle(absoluteBearing -
                         getGunHeadingRadians()));
-        fireBullet(3);
-
+        fire(2);
     }
 
     public void onHitByBullet(HitByBulletEvent e) {
-        turnLeft(90 - e.getBearing());
+        back(30);
+        setTurnLeft(30);
     }
-
 }
